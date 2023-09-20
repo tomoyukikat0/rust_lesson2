ce4286ee24809d7811391668f8dcc11e39d1357d
phwork12@gmail.com <phwork12@gmail.com>
2023-09-20 23:20:09 +0900

    add method

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
+fn say_hello() {
     println!("Hello, world!");
+}
+
+fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+fn main() {
+    say_hello();
+    println!("1 + 2 = {}", add(1, 2));
 }
