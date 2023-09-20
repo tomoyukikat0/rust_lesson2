67670ca00c40c136087df01fc2a86a5471d428f5
phwork12@gmail.com <phwork12@gmail.com>
2023-09-21 00:01:08 +0900

    match

--- src/main.rs
+++ src/main.rs
@@ -17,18 +17,31 @@
 
 use std::ops::RangeInclusive;
 
+// fn fizzbuzz(end: i32) {
+//     let r: RangeInclusive<i32> = 1..=end;
+
+//     for x in r {
+//         if x % 3 == 0 && x % 5 == 0 {
+//             println!("FizzBuzz");
+//         } else if x % 3 == 0 {
+//             println!("Fizz");
+//         } else if x % 5 == 0 {
+//             println!("Buzz");
+//         } else {
+//             println!("{}", x);
+//         }
+//     }
+// }
+
 fn fizzbuzz(end: i32) {
     let r: RangeInclusive<i32> = 1..=end;
 
     for x in r {
-        if x % 3 == 0 && x % 5 == 0 {
-            println!("FizzBuzz");
-        } else if x % 3 == 0 {
-            println!("Fizz");
-        } else if x % 5 == 0 {
-            println!("Buzz");
-        } else {
-            println!("{}", x);
+        match x % 15 {
+            0 => println!("FizzBuzz"),
+            3 | 6 | 9 | 12 => println!("Fizz"),
+            5 | 10 => println!("Buzz"),
+            _ => println!("{}", x),
         }
     }
 }
