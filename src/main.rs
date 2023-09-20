0273903f2bce63779566530ac1e44e033ed889ef
phwork12@gmail.com <phwork12@gmail.com>
2023-09-20 23:59:29 +0900

    while!

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,26 @@
-fn fizzbuzz(end: i32) {
-    let mut x: i32 = 1;
+// fn fizzbuzz(end: i32) {
+//     let mut x: i32 = 1;
 
-    while x <= end {
+//     while x <= end {
+//         if x % 3 == 0 && x % 5 == 0 {
+//             println!("FizzBuzz");
+//         } else if x % 3 == 0 {
+//             println!("Fizz");
+//         } else if x % 5 == 0 {
+//             println!("Buzz");
+//         } else {
+//             println!("{}", x);
+//         }
+//         x += 1;
+//     }
+// }
+
+use std::ops::RangeInclusive;
+
+fn fizzbuzz(end: i32) {
+    let r: RangeInclusive<i32> = 1..=end;
+
+    for x in r {
         if x % 3 == 0 && x % 5 == 0 {
             println!("FizzBuzz");
         } else if x % 3 == 0 {
@@ -11,7 +30,6 @@ fn fizzbuzz(end: i32) {
         } else {
             println!("{}", x);
         }
-        x += 1;
     }
 }
 
